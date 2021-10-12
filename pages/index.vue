620c070c5bc2764dea40fee2f2bb339fd346e3d3
andrusjakobson <andrusjakobson@hotmail.com>
2021-10-12 03:02:39 +0300

    Book detail view is mostly in-shape, resize image, justify-start for author and title, justify-end for availability and button

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,30 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12" sm="8" md="8">
       <h1>Raamatud</h1>
       <div>
-        <v-list v-for="book in this.books" three-line>
-          <v-list-item>
-            <v-img :src="book['']" />
+        <v-list
+          v-for="book in this.books"
+          :key="book['id']"
+          class="d-flex"
+          three-line>
+          <v-list-item class="flex-column justify-center">
+            <v-img :src="imageBaseUrl + book['pildiNimi']"/>
           </v-list-item>
-          <v-list-item>
-
+          <v-list-item class="flex-column justify-center">
+            <div class="justify-start">
+              <h3>{{ book['pealkiri'] }}</h3>
+              <p class="justify-start">{{ book['autor'] }}</p>
+            </div>
           </v-list-item>
-          <v-list-item>
-
+          <v-list-item class="flex-column justify-center">
+            <p>Saadavus: {{ book['saadavus'] }} tk</p>
+            <v-btn
+              color="primary"
+              elevation="2"
+              class="justify-end"
+            >osta
+            </v-btn>
           </v-list-item>
         </v-list>
       </div>
@@ -20,21 +33,20 @@
 </template>
 <script>
 
-
 export default {
   data() {
     return {
-      books: []
+      books: [],
+      imageBaseUrl: process.env.API_URL,
     }
   },
   mounted() {
-
-    this.$axios.get('/raamatud').then(res => {
+    this.$axios.get('raamatud').then(res => {
       if (res && res.data) {
         console.log(res);
         this.books = res.data;
       }
     })
-  }
+  },
 }
 </script>
