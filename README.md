fa6fefde81dc03263375728c81599389394f2b23
andrusjakobson <andrusjakobson@hotmail.com>
2021-10-14 05:28:26 +0300

    Update readme and test for button click

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # smit
 
-## Build Setup
-docker-compose up
+## With docker and docker-compose
+docker-compose up -d  
+Open localhost:3000
+
+### Run tests
+sudo docker exec -it smit npm run test
+
+## Without docker
+npm install
+
+### Run tests
+npm run test
--- test/pages.index.spec.js
+++ test/pages.index.spec.js
@@ -56,31 +56,39 @@ describe('index.vue', () => {
 
   it('should fill table with correct amount of books', async () => {
     await wrapper.vm.getBooks();
-    expect(wrapper.vm.books.length).toEqual(mockBooks.length)
+    expect(wrapper.vm.books.length).toEqual(mockBooks.length);
   });
 
   it('should have all the headlines in order', async () => {
     await wrapper.vm.getBooks();
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(1)').text()).toContain('TestHeadline')
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(2)').text()).toContain('The Tempest')
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(3)').text()).toContain('12 Rules for Life: An Antidote to Chaos')
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(4)').text()).toContain('The Call of Cthulhu')
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(1)').text()).toContain('TestHeadline');
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(2)').text()).toContain('The Tempest');
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(3)').text()).toContain('12 Rules for Life: An Antidote to Chaos');
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(4)').text()).toContain('The Call of Cthulhu');
   });
 
   it('should have all the authors in order', async () => {
     await wrapper.vm.getBooks();
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(1)').text()).toContain('TestAutor')
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(2)').text()).toContain('William Shakespeare')
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(3)').text()).toContain('Jordan B. Peterson')
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(4)').text()).toContain('H. P. Lovecraft')
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(1)').text()).toContain('TestAutor');
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(2)').text()).toContain('William Shakespeare');
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(3)').text()).toContain('Jordan B. Peterson');
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(4)').text()).toContain('H. P. Lovecraft');
   });
 
   it('should have all the availability in order', async () => {
     await wrapper.vm.getBooks();
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(1)').text()).toContain('Saadavus: 0 tk')
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(2)').text()).toContain('Saadavus: 5 tk')
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(3)').text()).toContain('Saadavus: 10 tk')
-    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(4)').text()).toContain('Saadavus: 15 tk')
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(1)').text()).toContain('Saadavus: 0 tk');
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(2)').text()).toContain('Saadavus: 5 tk');
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(3)').text()).toContain('Saadavus: 10 tk');
+    expect(wrapper.find('.v-data-table__wrapper tr:nth-child(4)').text()).toContain('Saadavus: 15 tk');
+  });
+
+  it('should console.log on button click', async () => {
+    await wrapper.vm.getBooks();
+    console.log = jest.fn();
+
+    expect(wrapper.find('.v-data-table__wrapper button').trigger('click'));
+    expect(console.log).toHaveBeenLastCalledWith('Osta: TestHeadline');
   });
 });
 
